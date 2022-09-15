b923d2b2546d968b34f4b27a68e6f236de23d90f
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-15 12:57:03 +0600

    para

---
+++ function.js
@@ -0,0 +1,8 @@
+//function declaration
+function startfan() {
+    console.log('Stand Up');
+    console.log('walk towards the switch');
+    console.log('press the switch');  
+}
+// call the function
+startfan();
---
+++ parameter.js
@@ -0,0 +1,18 @@
+function bringsingara(money){
+    console.log('taka den: ',money);
+    console.log('ei nen singara');
+}
+var taka=300
+// bringsingara(taka);
+
+function add (num1,num2){
+    console.log('going to add:',num1,num2);
+}
+// add(124,56);
+function sum(a,b,c,d,e) {
+    console.log(a,b,c,d,e);
+    var sum = a+b+c+d+e;
+
+    console.log(sum)
+}
+sum(15,98,56,5,9);
